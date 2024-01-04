474607fb100d13d0ca5216b3b41434e6e97b25fa
RespectDevelopment <143709835+respectdevelopment@users.noreply.github.com>
2024-01-04 18:36:28 +0100

    Update fxmanifest.lua

--- rd_DiscordRPC/fxmanifest.lua
+++ rd_DiscordRPC/fxmanifest.lua
@@ -1,10 +1,13 @@
+-- {{FX Infomration}}
 fx_version 'cerulean'
 game 'gta5'
 
+-- {{Resouce Information}}
 author 'Respect Development'
 description 'Best discord rpc script.'
 version '1.0.0'
 
+-- {{Manifest}} --
 lua54 'yes'
 
 shared_scripts {
